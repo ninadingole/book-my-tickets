c1572c5937c295c80d2bead8c54c593eb22a7b3a
Robert A. Wallis <robert.wallis@example.com>
2018-11-17 20:02:25 +0530

    Initial Modules

---
+++ src/main/java/com/iamninad/moviedemo/moviedemospring/MovieDemoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.iamninad.moviedemo.moviedemospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDemoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDemoSpringApplication.class, args);
+    }
+}
